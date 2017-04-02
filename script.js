0b64f04bf5c2185dca4cd5bc2c1266e2df1b220d
Reggie Bingham <reggiebing11@gmail.com>
2017-04-02 00:30:47 -0700

    8th commit added js

---
+++ script.js
@@ -0,0 +1,9 @@
+function lineThrough ();  {
+
+var myCheckBoxes = document.getElementByTagName("checkbox");
+var myLabels = document.getElementByTagName("label");
+
+if myCheckBoxes.checked {myLabels.add(text-decoration;)
+   }
+
+}
