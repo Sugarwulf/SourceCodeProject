36cd79364f5fed7655a647a3d2371d031492c563
Reggie Bingham <reggiebing11@gmail.com>
2017-04-02 15:05:01 -0700

    9th commit fix errors

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function lineThrough ();  {
 var myCheckBoxes = document.getElementByTagName("checkbox");
 var myLabels = document.getElementByTagName("label");
 
-if myCheckBoxes.checked {myLabels.add(text-decoration;)
+if myCheckBoxes.checked {myLabels.add(text-decoration: line-through;)
+} else {myLabels.remove(text-decoration: line-through;)
    }
 
 }
