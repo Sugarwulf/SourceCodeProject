f9bd260d184037cb0737deafca3c49371ee49a6e
Reggie Bingham <reggiebing11@gmail.com>
2017-04-03 20:12:08 -0700

    12th commit - fix js

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-function lineThrough();  {
+/*function lineThrough();  {
 
-var myCheckBoxes = document.getElementByTagName("checkbox").classList;
+var myCheckBoxes = document.getElementByTagName("checkbox");
 var myLabels = document.getElementByTagName("label").classList;
 
 if myCheckBoxes.checked {myLabels.add("line-through");
 } else {myLabels.remove("line-through");
    }
 
+} */
+
+function lineThrough(id) {
+  var mylabel = document.getElementById(id);
+
+  mylabel.style = "text-decoration: line-through";
 }
+
+
+/*function lineThrough (id) {
+var myCheckBoxes = document.getElementByTagName("checkbox");
+var mylabel = document.getElementById(id);
+
+if myCheckBoxes.checked {return myLabels.style = "text-decoration: line-through";
+   } else {return myLabels.style = "text-decoration: none";}
+
+} */
